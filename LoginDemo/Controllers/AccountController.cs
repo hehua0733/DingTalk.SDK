4343c16202cac2ddd0ce6f9abc74a89c4b366820
doff <fengchun_doff@sina.com>
2015-11-10 18:00:21 +0800

    update

--- LoginDemo/Controllers/AccountController.cs
+++ LoginDemo/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Gma.QrCodeNet.Encoding;
 using Gma.QrCodeNet.Encoding.Windows.Render;
+using LoginDemo.Models;
 using LoginDemo.Utils;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,7 @@ using System.Web.Security;
 
 namespace LoginDemo.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController : AsyncController
     {
         // GET: Login
         public ActionResult Index()
@@ -32,10 +33,135 @@ namespace LoginDemo.Controllers
             }
             else
             {
+
+                ViewBag.Id = Guid.NewGuid();
+                ViewBag.Qr = Base64.Base64Encode(ViewBag.Id + "");
+
+                QRScanModel qm = new QRScanModel { Id = ViewBag.Id, IsComfirm = false, Qr = Base64.Base64Encode(ViewBag.Id.ToString()) };
+                qm.ExpiredTime = DateTime.Now.AddMilliseconds(300000);
+                CacheUtil.PutCache("T_" + qm.Id, qm, 300000);
+
+
                 return View();
             }
         }
+        public ActionResult QR(string id)
+        {
+            QrEncoder encoder = new QrEncoder(ErrorCorrectionLevel.L);
+            QrCode qrCode;
+            encoder.TryEncode("http://mobiledev.scrzjh.com:9999/l/" + id.ToString(), out qrCode);
+            MemoryStream ms = new MemoryStream();
+            var render = new GraphicsRenderer(new FixedModuleSize(8, QuietZoneModules.Two));
+            render.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
+            var qm = CacheUtil.GetCache<QRScanModel>("T_" + Base64.Base64Decode(id));
+            if (qm != null)
+            {
+                qm.QrGenerateTime = DateTime.Now;
+                TimeSpan expiredTime = qm.ExpiredTime - DateTime.Now;
+                CacheUtil.PutCache("T_" + qm.Id, qm, (int)expiredTime.TotalMilliseconds);
+            }
+            return File(ms.GetBuffer(), "image/png");
+        }
 
+        #region 轮询长链接
+
+
+        // 处理客户端发起的请求
+        public void PollingAsync(Guid id, bool isScaned = false)
+        {
+
+
+            //计时器，5秒种触发一次Elapsed事件
+            System.Timers.Timer timer = new System.Timers.Timer(5000);
+            //告诉ASP.NET接下来将进行一个异步操作
+            AsyncManager.OutstandingOperations.Increment();
+            //订阅计时器的Elapsed事件
+            timer.Elapsed += (sender, e) =>
+            {
+
+                //打开一个监听 ，发现参数变化时，立即返回值?????
+
+
+                AsyncManager.Parameters["id"] = id;
+                AsyncManager.Parameters["timer"] = sender as System.Timers.Timer;
+
+                AsyncManager.OutstandingOperations.Decrement();
+            };
+
+            //启动计时器
+            timer.Start();
+
+            var qm = CacheUtil.GetCache<QRScanModel>("T_" + id);
+            if (qm != null && timer == null)
+            {
+                qm.PropertyChanged += (sender, e) =>
+                {
+                    AsyncManager.OutstandingOperations.Decrement();
+                };
+
+                qm.Timer = timer;
+                TimeSpan expiredTime = qm.ExpiredTime - DateTime.Now;
+                CacheUtil.PutCache("T_" + qm.Id, qm, (int)expiredTime.TotalMilliseconds);
+            }
+
+
+        }
+
+        //异步处理完成，向客户端发送响应
+        public ActionResult PollingCompleted(Guid id, System.Timers.Timer timer)
+        {
+
+
+            var qm = CacheUtil.GetCache<QRScanModel>("T_" + id);
+
+            if (qm != null)
+            {
+                if (qm.IsScan)
+                {
+                    return Json(new
+                    {
+                        status = 201,
+
+                    }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    if (DateTime.Now - qm.QrGenerateTime > new TimeSpan(0, 1, 0))
+                    {
+                        //更新QR内容
+                        CacheUtil.RemoveCache(qm.Id + "");
+                        qm.Id = Guid.NewGuid();
+                        qm.Qr = Base64.Base64Encode(qm.Id.ToString());
+                        qm.QrGenerateTime = DateTime.Now;
+                        TimeSpan expiredTime = qm.ExpiredTime - DateTime.Now;
+                        CacheUtil.PutCache("T_" + qm.Id, qm, (int)expiredTime.TotalMilliseconds);
+
+                        return Json(new
+                        {
+                            status = 409,
+                            id = qm.Id,
+                            qr = qm.Qr
+
+                        }, JsonRequestBehavior.AllowGet);
+                    }
+
+
+                    return Json(new
+                    {
+                        status = 408,
+
+                    }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+            return Json(new
+            {
+                status = 400,
+
+            }, JsonRequestBehavior.AllowGet);
+
+        }
+        #endregion
 
 
         /// <summary>
@@ -71,17 +197,21 @@ namespace LoginDemo.Controllers
         /// <returns></returns>
         public ActionResult LoginWithQR(string q, string state = "", string code = "")
         {
-            //二维码是否失效
-            Guid g = CacheUtil.GetCache<Guid>("T_" + q);
-            if (g == null || Guid.Empty == g)
-            {
-                return View();
-            }
+
 
 
 
             if (string.IsNullOrEmpty(state))
             {
+
+                //二维码是否失效
+                QRScanModel g = CacheUtil.GetCache<QRScanModel>("T_" + Base64.Base64Decode(q));
+                if (g == null)
+                {
+                    return View("empty");
+                }
+
+
                 var url = string.Format("https://oapi.dingtalk.com/connect/oauth2/authorize?appid={0}&redirect_uri={1}&response_type=code&scope=SCOPE&state=STATE",
                     CORPID,
                     Server.UrlEncode("http://mobiledev.scrzjh.com:9999/l/" + q)
@@ -100,22 +230,20 @@ namespace LoginDemo.Controllers
                 }
             }
 
-
             return View();
         }
 
-        public ActionResult QR()
+        public ActionResult Test(Guid id)
         {
+            QRScanModel g = CacheUtil.GetCache<QRScanModel>("T_" + id);
 
-            Guid g = Guid.NewGuid();
-            CacheUtil.PutCache("T_" + g, g, 60000);
-            QrEncoder encoder = new QrEncoder(ErrorCorrectionLevel.L);
-            QrCode qrCode;
-            encoder.TryEncode("http://mobiledev.scrzjh.com:9999/l/" + g.ToString(), out qrCode);
-            MemoryStream ms = new MemoryStream();
-            var render = new GraphicsRenderer(new FixedModuleSize(8, QuietZoneModules.Two));
-            render.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
-            return File(ms.GetBuffer(), "image/png");
+            if (g != null)
+                g.IsScan = true;
+
+            return Content("");
         }
+
+
+
     }
 }
---
+++ LoginDemo/Models/QRScanModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace LoginDemo.Models
+{
+    public class QRScanModel: INotifyPropertyChanged
+    {
+        private bool _isScan;
+        public bool IsScan
+        {
+            get { return _isScan; }
+            set {
+                if (_isScan!=value)
+                {
+                    _isScan = value;
+                    if (PropertyChanged!=null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("IsScan"));
+                    }
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public QRScanModel()
+        {
+            CreateTime = DateTime.Now;
+        }
+        public Guid Id { get; set; }
+        public string Qr { get; set; }
+        public DateTime QrGenerateTime { get; set; }
+        public DateTime ExpiredTime { get; set; }
+        public DateTime CreateTime { get; private set; }
+        public System.Timers.Timer Timer { get; set; }
+        public bool IsComfirm { get; set; }
+    }
+}
