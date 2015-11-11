8e009b8fad3acc8535d4a7f4d5c2912580b5a0ab
doff <fengchun_doff@sina.com>
2015-11-11 17:54:37 +0800

    update

--- LoginDemo/Controllers/AccountController.cs
+++ LoginDemo/Controllers/AccountController.cs
@@ -1,6 +1,5 @@
 ﻿using Gma.QrCodeNet.Encoding;
 using Gma.QrCodeNet.Encoding.Windows.Render;
-using LoginDemo.Models;
 using LoginDemo.Utils;
 using System;
 using System.Collections.Generic;
@@ -13,8 +12,14 @@ using System.Web.Security;
 
 namespace LoginDemo.Controllers
 {
-    public class AccountController : AsyncController
+    public class AccountController : Controller
     {
+
+        private readonly string CORPID = System.Configuration.ConfigurationManager.AppSettings["DD_CORPID"] + "";
+        private readonly string CORPSECRET = System.Configuration.ConfigurationManager.AppSettings["DD_CORPSECRET"] + "";
+
+
+
         // GET: Login
         public ActionResult Index()
         {
@@ -33,15 +38,6 @@ namespace LoginDemo.Controllers
             }
             else
             {
-
-                ViewBag.Id = Guid.NewGuid();
-                ViewBag.Qr = Base64.Base64Encode(ViewBag.Id + "");
-
-                QRScanModel qm = new QRScanModel { Id = ViewBag.Id, IsComfirm = false, Qr = Base64.Base64Encode(ViewBag.Id.ToString()) };
-                qm.ExpiredTime = DateTime.Now.AddMilliseconds(300000);
-                CacheUtil.PutCache("T_" + qm.Id, qm, 300000);
-
-
                 return View();
             }
         }
@@ -49,145 +45,26 @@ namespace LoginDemo.Controllers
         {
             QrEncoder encoder = new QrEncoder(ErrorCorrectionLevel.L);
             QrCode qrCode;
-            encoder.TryEncode("http://mobiledev.scrzjh.com:9999/l/" + id.ToString(), out qrCode);
+            encoder.TryEncode("http://mobiledev.scrzjh.com:9999/l?id=" + id.ToString(), out qrCode);
             MemoryStream ms = new MemoryStream();
             var render = new GraphicsRenderer(new FixedModuleSize(8, QuietZoneModules.Two));
             render.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
-            var qm = CacheUtil.GetCache<QRScanModel>("T_" + Base64.Base64Decode(id));
-            if (qm != null)
-            {
-                qm.QrGenerateTime = DateTime.Now;
-                TimeSpan expiredTime = qm.ExpiredTime - DateTime.Now;
-                CacheUtil.PutCache("T_" + qm.Id, qm, (int)expiredTime.TotalMilliseconds);
-            }
+    
             return File(ms.GetBuffer(), "image/png");
         }
 
-        #region 轮询长链接
-
-
-        // 处理客户端发起的请求
-        public void PollingAsync(Guid id, bool isScaned = false)
-        {
-
-
-            //计时器，5秒种触发一次Elapsed事件
-            System.Timers.Timer timer = new System.Timers.Timer(5000);
-            //告诉ASP.NET接下来将进行一个异步操作
-            AsyncManager.OutstandingOperations.Increment();
-            //订阅计时器的Elapsed事件
-            timer.Elapsed += (sender, e) =>
-            {
-
-                //打开一个监听 ，发现参数变化时，立即返回值?????
-
-
-                AsyncManager.Parameters["id"] = id;
-                AsyncManager.Parameters["timer"] = sender as System.Timers.Timer;
-
-                AsyncManager.OutstandingOperations.Decrement();
-            };
-
-            //启动计时器
-            timer.Start();
-
-            var qm = CacheUtil.GetCache<QRScanModel>("T_" + id);
-            if (qm != null && timer == null)
-            {
-                qm.PropertyChanged += (sender, e) =>
-                {
-                    AsyncManager.OutstandingOperations.Decrement();
-                };
-
-                qm.Timer = timer;
-                TimeSpan expiredTime = qm.ExpiredTime - DateTime.Now;
-                CacheUtil.PutCache("T_" + qm.Id, qm, (int)expiredTime.TotalMilliseconds);
-            }
-
-
-        }
-
-        //异步处理完成，向客户端发送响应
-        public ActionResult PollingCompleted(Guid id, System.Timers.Timer timer)
-        {
-
-
-            var qm = CacheUtil.GetCache<QRScanModel>("T_" + id);
-
-            if (qm != null)
-            {
-                if (qm.IsScan)
-                {
-                    return Json(new
-                    {
-                        status = 201,
-
-                    }, JsonRequestBehavior.AllowGet);
-                }
-                else
-                {
-                    if (DateTime.Now - qm.QrGenerateTime > new TimeSpan(0, 1, 0))
-                    {
-                        //更新QR内容
-                        CacheUtil.RemoveCache(qm.Id + "");
-                        qm.Id = Guid.NewGuid();
-                        qm.Qr = Base64.Base64Encode(qm.Id.ToString());
-                        qm.QrGenerateTime = DateTime.Now;
-                        TimeSpan expiredTime = qm.ExpiredTime - DateTime.Now;
-                        CacheUtil.PutCache("T_" + qm.Id, qm, (int)expiredTime.TotalMilliseconds);
-
-                        return Json(new
-                        {
-                            status = 409,
-                            id = qm.Id,
-                            qr = qm.Qr
-
-                        }, JsonRequestBehavior.AllowGet);
-                    }
-
-
-                    return Json(new
-                    {
-                        status = 408,
-
-                    }, JsonRequestBehavior.AllowGet);
-                }
-            }
-
-            return Json(new
-            {
-                status = 400,
-
-            }, JsonRequestBehavior.AllowGet);
-
-        }
-        #endregion
-
-
+      
         /// <summary>
         /// 这个页面是在手机上打开的
         /// </summary>
         /// <returns></returns>
         public ActionResult LoginConfirm()
         {
-            //FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1,
-            //   user.name,
-            //   DateTime.Now,
-            //   DateTime.Now,
-            //   false,
-            //   user.userid + "",
-            //   "/");
-            //var logincookie = new HttpCookie(FormsAuthentication.FormsCookieName, FormsAuthentication.Encrypt(ticket));
-            //logincookie.HttpOnly = true;
-            //HttpContext.Response.Cookies.Add(logincookie);
-
-            return View();
+                    return View();
         }
 
 
-        private readonly string CORPID = System.Configuration.ConfigurationManager.AppSettings["DD_CORPID"] + "";
-        private readonly string CORPSECRET = System.Configuration.ConfigurationManager.AppSettings["DD_CORPSECRET"] + "";
-
+    
         /// <summary>
         /// 这个页面是在手机上打开的
         /// </summary>
@@ -195,26 +72,14 @@ namespace LoginDemo.Controllers
         /// <param name="state"></param>
         /// <param name="code"></param>
         /// <returns></returns>
-        public ActionResult LoginWithQR(string q, string state = "", string code = "")
+        public ActionResult LoginWithQR(Guid id, string state = "", string code = "")
         {
-
-
-
-
             if (string.IsNullOrEmpty(state))
             {
 
-                //二维码是否失效
-                QRScanModel g = CacheUtil.GetCache<QRScanModel>("T_" + Base64.Base64Decode(q));
-                if (g == null)
-                {
-                    return View("empty");
-                }
-
-
                 var url = string.Format("https://oapi.dingtalk.com/connect/oauth2/authorize?appid={0}&redirect_uri={1}&response_type=code&scope=SCOPE&state=STATE",
                     CORPID,
-                    Server.UrlEncode("http://mobiledev.scrzjh.com:9999/l/" + q)
+                    Server.UrlEncode( "http://mobiledev.scrzjh.com:9999/l?id=" + id)
                     );
                 return Redirect(url);
             }
@@ -226,6 +91,7 @@ namespace LoginDemo.Controllers
 
                 if (user != null)
                 {
+                    ViewBag.ConnetionId = id;
                     return View("LoginConfirm", user);
                 }
             }
@@ -233,16 +99,7 @@ namespace LoginDemo.Controllers
             return View();
         }
 
-        public ActionResult Test(Guid id)
-        {
-            QRScanModel g = CacheUtil.GetCache<QRScanModel>("T_" + id);
-
-            if (g != null)
-                g.IsScan = true;
-
-            return Content("");
-        }
-
+       
 
 
     }
---
+++ LoginDemo/LoginWithQrHub.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace LoginDemo
+{
+    [HubName("QrHub")]
+    public class LoginWithQrHub : Hub
+    {
+        
+        public void Scaned(string pcConnectionId, string mobileConnectionId, string avatar, string username)
+        {
+            Clients.Client(pcConnectionId).LetConfirm(avatar, username);
+        }
+        public void Confirm(string pcConnectionId, string mobileConnectionId, string userid)
+        {
+            Clients.Client(pcConnectionId).Login(userid);
+
+            Clients.Client(pcConnectionId).Disconnect();
+            Clients.Client(mobileConnectionId).Disconnect();
+        }
+    }
+}
